4490d58826df542ec32ac0f2d18f39b5778d5de5
Jason Moiron <jmoiron@jmoiron.net>
2019-11-19 22:27:42 -0500

    start the big rewrite by getting rid of global config

--- monet.go
+++ monet.go
@@ -1,50 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
+	"flag"
+	"net/http"
 
-	"github.com/hoisie/web"
-	"github.com/jmoiron/monet/blog"
+	"github.com/gorilla/handlers"
 	"github.com/jmoiron/monet/conf"
-	"github.com/jmoiron/monet/db"
-	"github.com/jmoiron/monet/template"
-	// apps
-	"github.com/jmoiron/monet/app"
-	"github.com/jmoiron/monet/gallery"
 )
 
 func main() {
-	fmt.Printf("Using config %s\n", conf.Path)
-	fmt.Printf("Using models:\n")
-	for _, m := range db.Models {
-		t := reflect.TypeOf(m)
-		fmt.Printf("    %s\n", fmt.Sprintf("%s", t)[1:])
-	}
 
-	template.Init(conf.Config.TemplatePaths)
-	fmt.Printf(conf.Config.String())
-	web.Config.StaticDir = conf.Config.StaticPath
+	flag.BoolVar(&conf.Config.Debug, "debug", false, "enable debug mode")
 
-	db.Connect(conf.Config.DbHostString(), conf.Config.DbName)
-	db.RegisterAllIndexes()
-
-	blog.AttachAdmin("/admin/")
-	blog.Attach("/")
-
-	app.AttachAdmin("/admin/")
-	app.Attach("/")
-
-	gallery.AttachAdmin("/admin/")
-	gallery.Attach("/")
-
-	web.Get("/$", blog.Index)
-	web.Get("/(.*)", blog.Flatpage)
-
-	app.AddPanel(&blog.PostPanel{})
-	app.AddPanel(&blog.UnpublishedPanel{})
-	app.AddPanel(&blog.PagesPanel{})
-	app.AddPanel(&gallery.GalleryPanel{})
-
-	web.Run(conf.Config.HostString())
+	r := http.NewServeMux()
+	http.ListenAndServe(":8001", handlers.CompressHandler(r))
 }
